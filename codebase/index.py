fff9a1b40027ba08ecca5cd5d1ac76c412b1302e
Amey Bhogaonkar <amey.bhogaonkar@everestek.com>
2025-02-11 17:39:56 +0530

    Deletion from codebase

--- codebase/index.py
+++ codebase/index.py
@@ -1,34 +1,5 @@
 # print("Hello World This is CodeBase")
 print("This  is Sync Repo")
-print("This  is Sync Repo")
-print("This  is Sync Repo")
-print("This  is Sync Repo")
-print("This  is Sync Repo")
-print("This  is Sync Repo")
-print("This  is Sync Repo")
-print("This  is Sync Repo")
-print("This  is Sync Repo")
-print("This  is Sync Repo")
-print("This  is Sync Repo")
-print("This  is Sync Repo")
-print("This  is Sync Repo")
-print("This  is Sync Repo")
-print("This  is Sync Repo")
 
-# print("Hello World")
-
-# print("Hello World")
-# print("Hello World")
-
-# print("Hello World")
-# print("Hello World")
-# print("Hello World")
-# print("Hello World")
-
-print("This  is Sync Repo")
-print("This  is Sync Repo")
-
-print("This  is Sync Repo")
-print("This  is Sync Repo")
 
 
