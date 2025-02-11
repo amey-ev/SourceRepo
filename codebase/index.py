1ef9bede7b731628667bc500afad5d3fdce464d2
Amey Bhogaonkar <amey.bhogaonkar@everestek.com>
2025-02-11 16:50:29 +0530

    print("This  is Sync Repo") Fix:

--- codebase/index.py
+++ codebase/index.py
@@ -1,2 +1,8 @@
 # print("Hello World This is CodeBase")
 print("This  is Sync Repo")
+print("This  is Sync Repo")
+print("This  is Sync Repo")
+print("This  is Sync Repo")
+print("This  is Sync Repo")
+print("This  is Sync Repo")
+print("This  is Sync Repo")
